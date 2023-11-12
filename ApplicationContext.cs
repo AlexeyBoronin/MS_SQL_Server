b0ef8002afe1791b54af825ef6eae3a24e2de2b3
Alexey <boronin_1993@mail.ru>
2023-11-12 14:08:54 +0300

    Добавьте файлы проекта.

---
+++ ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS_SQL_Server
+{
+    public class ApplicationContext:DbContext
+    {
+        public DbSet<Human> Humans { get; set; } = null;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Test;Trusted_Connection=True");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using MS_SQL_Server;
+using(ApplicationContext db = new ApplicationContext())
+{
+    db.Database.EnsureDeleted();
+    db.Database.EnsureCreated();
+
+    Human human1 = new Human { Name="Ted",Age=40};
+    Human human2 = new Human { Name = "Jane", Age=30 };
+
+    db.Humans.AddRange(human1, human2);
+    db.SaveChanges();
+}
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    var humans=db.Humans.ToList();
+    Console.WriteLine("Human List:");
+    foreach(Human h in humans)
+        Console.WriteLine($"{h.Id}.{h.Name} - {h.Age}");
+}
